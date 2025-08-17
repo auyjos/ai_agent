85b42a78b7bf2eb78d6eb6fb17397a6eaee20b60
auyjos <josea13p@gmail.com>
2025-08-16 21:36:28 -0600

    implemented agentic mode to determine function run

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,7 @@
 import os
 
+from google.genai import types
+
 from config import MAX_CHARS
 
 
@@ -29,3 +31,17 @@ def get_file_content(working_directory, file_path):
     except Exception as e:
         # Catch any unexpected errors and return them as a string
         return f"Error: {str(e)}"
+    
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Read the contents of a file relative to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file relative to the working directory."
+            )
+        }
+    )
+)
